4ad7e043ba567bc7cbcf83fab791270c228d07b2
Ali Aliyev <elvinaliyev724@gmail.com>
2024-05-14 11:49:21 +0400

    Finish

---
+++ MVC example/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_example.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Ali";
+            ViewBag.SurName = "Aliyev";
+            ViewBag.Age = 21;
+            ViewBag.Country = "Azerbaijan";
+            ViewBag.PhoneNumber = 0777777777;
+
+
+            return View();
+        }
+    }
+}
